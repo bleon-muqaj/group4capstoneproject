3691f20bf05099fa5c4bedc504adbfde4ecc0203
JavierGolpeJ10 <golpeju2@uwm.edu>
2025-02-24 20:49:07 -0600

    Added page numbers and new instructions
    not much done added all the working instructions to show on the side of the pdf

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,23 @@ import './Header.css';
 function Header() {
     const [pdfOpen, setPdfOpen] = useState(false);
     const chapters = [
-        { title: "Introduction", page: 1 },
-        { title: "Setup", page: 5 },
-        { title: "Instructions", page: 10 },
-        { title: "Advanced Topics", page: 15 },
+        { title: "Table of Contents", page: 1 },
+        // { title: "Setup", page: 5 },
+        { title: "Instructions", page: 42 },
+        // { title: "Advanced Topics", page: 14 },
+        { title: "addi", page: 45},
+        { title: "addiu", page: 46},
+        { title: "addu", page: 48},
+        { title: "andi", page: 55},
+        { title: "beq", page: 82},
+        { title: "bne", page: 113},
+        { title: "j", page: 204},
+        { title: "or", page: 324},
+        { title: "ori", page: 325},
+        { title: "sll", page: 386},
+        { title: "slt", page: 388},
+        { title: "syscall", page: 425},
+        { title: "xor", page: 456},
     ];
 
     return (
